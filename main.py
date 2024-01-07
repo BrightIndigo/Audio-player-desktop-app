6f7bacd3b2b240d3dcfd37a81f1834965982bbcc
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2024-01-07 22:45:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from tkinter import *
 from tkinter import ttk 
-import wave
+import os
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Audio Player").grid(column=0, row=0)
+frm.grid(row=0, column=0, sticky=(N, W, E, S))
+
+ttk.Label(frm, text="Audio Player").grid(row=0, column=0, columnspan=2)
 
 def play():
-    print("sth")
+    # Replace this with the actual implementation to play audio
+    print("Playing audio...")
 
+ttk.Button(frm, text="Play", command=play).grid(row=1, column=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(row=0, column=1)
 
-ttk.Button(frm, text="Play", command=play()).grid(column=0, row=1)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+def print_answers():
+    print("sanie")
+
+submit_button = Button(root, text='Submit', command=print_answers) 
+submit_button.grid(row=2, column=0, columnspan=2)
+
 root.mainloop()
-
-
